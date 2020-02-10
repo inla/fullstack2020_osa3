65c0b1bbda9208644db38a1e16721105b75a16cb
Matias Rajamäki <inka.lappalainen@hotmail.com>
2020-02-10 20:46:11 +0200

    heroku yritystä

---
+++ build/precache-manifest.bb25897662a234495b23d5d4c64dec20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53b3f14cd74849c80d47158206ae21bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "669d7dac019fd4aa33df",
+    "url": "/static/css/main.096542e1.chunk.css"
+  },
+  {
+    "revision": "a4ff7f15b0a9c86bc646",
+    "url": "/static/js/2.b60c7687.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b60c7687.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "669d7dac019fd4aa33df",
+    "url": "/static/js/main.29121637.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
